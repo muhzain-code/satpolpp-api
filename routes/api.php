6141b5dcda7dac95017b5b23c2f6d62fe9fa2f46
muhzain <mzhasan1404@gmail.com>
2025-11-06 12:34:39 +0700

    menerapkan auth dan role di routes api

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,10 @@ Route::get('/user', function (Request $request) {
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-Route::get('jabatan', [JabatanController::class, 'index']);
-Route::post('jabatan', [JabatanController::class, 'store']);
-Route::get('jabatan/{id}', [JabatanController::class, 'show']);
-Route::put('jabatan/{id}', [JabatanController::class, 'update']);
-Route::delete('jabatan/{id}', [JabatanController::class, 'destroy']);
+Route::middleware('auth:sanctum', 'role:super_admin')->group(function () {
+    Route::get('jabatan', [JabatanController::class, 'index']);
+    Route::post('jabatan', [JabatanController::class, 'store']);
+    Route::get('jabatan/{id}', [JabatanController::class, 'show']);
+    Route::put('jabatan/{id}', [JabatanController::class, 'update']);
+    Route::delete('jabatan/{id}', [JabatanController::class, 'destroy']);
+});
